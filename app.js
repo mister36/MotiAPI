830c1bc235675086b5a986687876b03fd0ae6278
Macbook <macbook@adamselitecompu.attlocal.net>
2020-11-27 19:49:31 -0600

    Added push notifications for iOS

--- app.js
+++ app.js
@@ -36,28 +36,41 @@ app.get("/test-send", async (req, res) => {
   const notification = new NotificationService();
 
   const registrationIds = [
-    "e5GfdhGkR-Ca-jDnXwFjrJ:APA91bF5he4my6jNjUv5MWXzrA2MdoMb8f_yBtUf92j1PmrzUcnnWxNTEclkHV8JYPA7-deAnB8Ppyk_de2vmgIHm7pKkRs-D2AYPZvmlDe7Ic4pNZM-tEid_Epj5NhCeIda8ChfLNLL",
+    "evBUOOJJTZihOH5P57yLS4:APA91bH0Mvru5rmKAhOvdgQWMUGHcel3sU8N2iWiH160sI_nLyhXxUKKB4e9cDYKxTtCVJytObBHSYlez2a3Uiak8rzMomXDmF9uy6jBv42lf36gcpBQ_RSYYBNDu-WNcJJ72dOhksAp",
+    "9b679e219d54c5a703643ad4dc7b4caf5602add7e914a55a4ffc6202a5af755c",
   ];
 
+  const data = {
+    title: "This",
+    body: "Is amazing. So glad it works",
+    topic: "com.motisesh.motisesh",
+    custom: {
+      sender: "Motisesh",
+    },
+    icon: "ic_icon",
+    color: "white",
+    priority: "high",
+    android_channel_id: "4323",
+  };
+
   notification
-    .send(registrationIds, {
-      title: "Testing Moti noti",
-      body: "I'm so glad this worked",
-      custom: {
-        sender: "Motisesh",
-      },
-      priority: "high",
-      android_channel_id: "4323",
-    })
+    .send(registrationIds, data)
     .then((results) => {
-      console.log(results);
-      console.log(results[0].message);
+      // console.log(results);
+      // console.log(results[0].message);
+      res.status(200).json({
+        results,
+        // message: results[0].message,
+      });
     })
     .catch((err) => {
       console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
 
-  res.end("Done");
+  // res.end("Done");
 });
 
 // !Error handling
--- pushNotifications.js
+++ pushNotifications.js
@@ -1,20 +1,24 @@
 const PushNotifications = require("node-pushnotifications");
+const jwt = require("jsonwebtoken");
+
+// const token = jwt.sign({});
 
 class NotificationService {
   constructor() {
     this.config = {
-      //   apn: {
-      //     token: {
-      //       key: process.env.APNS_P8.replace(/\\n/g, "\n"),
-      //       keyId: process.env.APNS_KEY_ID,
-      //       teamId: process.env.APNS_TEAM_ID,
-      //     },
-      //     production: false,
-      //   },
+      apn: {
+        token: {
+          // key: process.env.APNS_P8.replace(/\\n/g, "\n"),
+          key: `${__dirname}/${process.env.APN_KEY_FILE_NAME}`,
+          keyId: process.env.APN_KEY_ID,
+          teamId: process.env.APN_TEAM_ID,
+        },
+        production: process.env.NODE_ENV === "production" ? true : false,
+      },
       gcm: {
         id: process.env.FCM_API_KEY,
       },
-      isAlwaysUseFCM: true,
+      // isAlwaysUseFCM: true,
     };
     this.push = new PushNotifications(this.config);
   }
