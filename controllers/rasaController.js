72a499c3dfb289dca6142d85a7de0522267d9a43
Adam Achebe <mister363636@gmail.com>
2020-12-19 21:24:46 -0600

    Added RASA action

--- controllers/rasaController.js
+++ controllers/rasaController.js
@@ -1,21 +1,28 @@
 const { handleFormAction } = require("nodejs-formaction-sdk-rasa");
 const _ = require("lodash");
 const quotes = require("../static_files/quotes.json");
+const fs = require("fs");
 
 exports.action = (req, res, next) => {
   const request = req.body;
   const next_action = request.next_action;
+  const events = request.tracker.events;
   const entities = request.tracker.latest_message.entities;
   const slots = request.tracker.slots;
   const sender = request.sender_id;
+  const responses = request.domain.responses;
+  const active_form = request.tracker.active_form;
+
+  // send payload function
+  const send = (load) => {
+    res.status(200).json(load);
+  };
 
   // response payload, what will be sent back
   let resPayload = { events: [], responses: [] };
 
   switch (next_action) {
     case "action_grab_quote":
-      nextAction = "action_listen";
-
       // picks random quote
       const randomQuoteObj = quotes[_.random(0, quotes.length)];
       resPayload.responses.push({
@@ -23,55 +30,25 @@ exports.action = (req, res, next) => {
       });
 
       // send payload
-      res.status(200).json(resPayload);
+      send(resPayload);
+      break;
+
+    case "action_ask_record_motivation_form_actions_today":
+      const { number } = slots;
+
+      resPayload.events.push({
+        event: "slot",
+        name: "motivation_num",
+        value: number,
+      });
+      resPayload.responses.push({
+        text: "Gimme a summary of what you’ve done (or haven’t done) today",
+      });
+
+      send(resPayload);
       break;
     default:
-      console.log("Wrong action");
+      console.log("WRONG ACTION /////////////////////////");
+      console.log(next_action);
   }
 };
-
-// exports.action = (req, res, next) => {
-//   const request = req.body;
-//   const next_action = request.next_action;
-//   const entities = request.tracker.latest_message.entities;
-//   const slots = request.tracker.slots;
-//   const sender = request.sender_id;
-//   // These are all the slots
-//   //   const required_slots = [
-//   //     "toppings",
-//   //     "radius",
-//   //     "boxes",
-//   //     "enemy_name",
-//   //     "phone_number",
-//   //   ];
-
-//   let formAction;
-//   let nextAction;
-
-//   // Get rid of this later
-//   //   console.log(request);
-
-//   switch (next_action) {
-//     case "action_say_form_finished":
-//       nextAction = "action_listen";
-//       formAction = handleFormAction(
-//         ["toppings", "radius", "boxes"],
-//         entities,
-//         slots,
-//         nextAction,
-//         sender
-//       );
-
-//       formAction
-//         .then((results) => {
-//           console.log("got results");
-//           console.log(results);
-//           return res.status(200).json(results);
-//         })
-//         .catch((err) => console.log(err));
-//       break;
-
-//     default:
-//       console.log("No action");
-//   }
-// };
---
+++ utils/formValidation.js
@@ -0,0 +1,10 @@
+const formValidation = (requiredSlots, domainSlots, customSlots) => {
+  // requiredSlots: what the form needs
+  // domainSlots: slots specified in the domain
+  // customSlots: must be mapped a different way
+  const resPayload = { events: [], responses: [] };
+  return new Promise((resolve, reject) => {
+    // text of last user input
+    const extractGoal = () => {};
+  });
+};
